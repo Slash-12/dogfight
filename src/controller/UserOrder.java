42c912f26c75a7ffefe76f92ef354e5bc08d40d8
Slash-12 <baptiste.moench@viacesi.fr>
2019-05-03 08:57:21 +0200

    Phase 3

--- src/controller/UserOrder.java
+++ src/controller/UserOrder.java
@@ -5,7 +5,7 @@ public class UserOrder implements IUserOrder {
 	private int player;
 	private Order order;
 
-	public UserOrder(int player, Order order){
+	public UserOrder(int player, Order order) {
 		this.player = player;
 	}
 
