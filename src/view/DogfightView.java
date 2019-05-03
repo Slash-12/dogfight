cac7b1be8fd6f4ca9aace3d071ed705059fbae51
jonah <jonah.wright@viacesi.fr>
2019-05-03 09:07:50 +0200

    une partie de la phase 3

--- src/view/DogfightView.java
+++ src/view/DogfightView.java
@@ -3,4 +3,19 @@ package view;
 public class DogfightView {
 
 
+    // private methods
+    private  void run(){
+
+    }
+
+    // public methods
+    public String displaymessage(String message){
+
+        return message;
+    }
+
+    public void closeAll(){
+
+    }
+
 }
--- src/view/EventPerformer.java
+++ src/view/EventPerformer.java
@@ -1,4 +1,23 @@
 package view;
 
+import controller.IOrderPerformer;
+import controller.UserOrder;
+
+import java.awt.event.KeyEvent;
+
 public class EventPerformer {
+
+    // private methods
+    private UserOrder keyCodeToUserOrder(int keyCode){
+
+        return null; // this probably won't be null
+    }
+
+    // public methods
+    public void eventPerform(KeyEvent keyCode){
+
+    }
+
+
+
 }
