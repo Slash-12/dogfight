f98ad83a4716ad4622f4891eb72f12ef53af57c5
Slash-12 <baptiste.moench@viacesi.fr>
2019-05-03 08:56:04 +0200

    Phase 2&3 LOLOLOLOLOLOL gné

--- src/controller/UserOrder.java
+++ src/controller/UserOrder.java
@@ -3,15 +3,20 @@ package controller;
 public class UserOrder implements IUserOrder {
 
 	private int player;
+	private Order order;
+
+	public UserOrder(int player, Order order){
+		this.player = player;
+	}
 
 	@Override
 	public Order getOrder() {
-		return null;	
+		return null;
 	}
 
 	@Override
 	public int getPlayer() {
-		return 0;
+		return 1;
 	}
 	
 	
